553ed7135ca4e6185c0f58eab191a3eac8186975
ayalsaad0 <ayalsaad0@gmail.com>
2022-06-02 19:17:52 +0300

    calendar (accessing the save button)

---
+++ client/src/components/EditorWindowTemplate.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
+import { DateTimePickerComponent } from "@syncfusion/ej2-react-calendars";
+import { TextBoxComponent } from "@syncfusion/ej2-react-inputs";
+
+const EditorWindowTemplate = (props) => {
+  const [id, setId] = useState("");
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [categoryColor, setCategoryColor] = useState("");
+  const [description, setDesciption] = useState("");
+
+  return props !== undefined ? (
+    <table
+      className="custom-event-editor"
+      style={{ width: "100%", cellpadding: "5" }}
+    >
+      <tbody>
+        <tr>
+          <td className="e-textlabel">Summary</td>
+          <td colSpan={4}>
+            <input
+              id="Summary"
+              className="e-field e-input"
+              type="text"
+              name="Subject"
+              style={{ width: "100%" }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td className="e-textlabel">Status</td>
+          <td colSpan={4}>
+            <DropDownListComponent
+              id="EventType"
+              placeholder="Choose status"
+              data-name="EventType"
+              className="e-field"
+              style={{ width: "100%" }}
+              dataSource={["New", "Requested", "Confirmed"]}
+              value={props.EventType || null}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td className="e-textlabel">From</td>
+          <td colSpan={4}>
+            <DateTimePickerComponent
+              format="dd/MM/yy hh:mm a"
+              id="StartTime"
+              data-name="StartTime"
+              value={new Date(props.startTime || props.StartTime)}
+              className="e-field"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td className="e-textlabel">To</td>
+          <td colSpan={4}>
+            <DateTimePickerComponent
+              format="dd/MM/yy hh:mm a"
+              id="EndTime"
+              data-name="EndTime"
+              value={new Date(props.endTime || props.EndTime)}
+              className="e-field"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td className="e-textlabel">Reason</td>
+          <td colSpan={4}>
+            <textarea
+              id="Description"
+              className="e-field e-input"
+              name="Description"
+              rows={3}
+              cols={50}
+              style={{
+                width: "100%",
+                height: "60px !important",
+                resize: "vertical",
+              }}
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  ) : (
+    <div />
+  );
+};
+
+export default EditorWindowTemplate;
--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -12,42 +12,62 @@ import {
   Resize,
   DragAndDrop,
 } from "@syncfusion/ej2-react-schedule";
-import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
-import FetchEvents from "../data/Events";
+import { L10n } from "@syncfusion/ej2-base";
 import { getEvents } from "../data/Events";
-
-import { scheduleData } from "../data/dummy";
 import { Header } from "../components";
-import TextField from "@mui/material/TextField";
-// import DateTimePickerComponent from "../components/DateTimePicker";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import EditorWindowTemplate from "../components/EditorWindowTemplate";
+import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
 import { DateTimePickerComponent } from "@syncfusion/ej2-react-calendars";
-import { TextBoxComponent } from "@syncfusion/ej2-react-inputs";
-import {
-  HtmlEditor,
-  Image,
-  Link,
-  QuickToolbar,
-  RichTextEditorComponent,
-  Toolbar,
-} from "@syncfusion/ej2-react-richtexteditor";
-import Editor from "../components/Editor";
+
+const onPopupOpen = (args) => {
+  if (args.type === "Editor") {
+    let statusElement = args.element.querySelector("#EventType");
+    statusElement.setAttribute("name", "EventType");
+  }
+};
+
+const onPopupClose = (args, { scheduleObj }) => {
+  if (args.type === "Editor") {
+    if (
+      ["Add", "Save", "EditSeries", "EditOccurrence"].indexOf(
+        this.scheduleObj.currentAction
+      ) > -1
+    ) {
+      console.log("first");
+    } else if (this.scheduleObj.currentAction === null) {
+      // Handle the code if "cancel" button is clicked.
+    }
+  }
+};
 
 // eslint-disable-next-line react/destructuring-assignment
 const PropertyPane = (props) => <div className="mt-5">{props.children}</div>;
 
+const handleClick = () => {
+  console.log("first");
+};
+
+// L10n.load({
+//   "en-US": {
+//     schedule: {
+//       saveButton: "Add",
+//       cancelButton: "Close",
+//       deleteButton: "Remove",
+//       newEvent: "Add Event",
+//     },
+//     onsubmit: handleClick,
+//   },
+// });
+
 const Scheduler = () => {
   const events = getEvents();
 
-  const [id, setId] = useState("");
-  const [subject, setSubject] = useState("");
-  const [location, setLocation] = useState("");
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
-  const [categoryColor, setCategoryColor] = useState("");
-  const [description, setDesciption] = useState("");
-
   const [scheduleObj, setScheduleObj] = useState();
+
+  const addEvent = (scheduleObj) => {
+    const length = scheduleObj.eventsData.length;
+    console.log(scheduleObj[length - 1]);
+  };
 
   const change = (args) => {
     scheduleObj.selectedDate = args.value;
@@ -59,34 +79,19 @@ const Scheduler = () => {
     arg.navigation.enable = true;
   };
 
-  const editorWindowTemplate = () => {
-    return (
-      <div>
-        <div>
-          <TextBoxComponent placeholder="Add title" floatLabelType="Auto" />
-          <TextBoxComponent placeholder="Location" floatLabelType="Auto" />
-          <DateTimePickerComponent placeholder="StartTime" />
-          <DateTimePickerComponent placeholder="EndTime" />
-          <TextBoxComponent
-            multiline={true}
-            placeholder="Add description"
-            floatLabelType="Never"
-          />
-        </div>
-      </div>
-    );
-  };
-
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <Header category="App" title="Calendar" />
       <ScheduleComponent
-        editorTemplate={editorWindowTemplate}
+        editorTemplate={EditorWindowTemplate}
+        popupOpen={onPopupOpen}
+        popupClose={(scheduleObj) => onPopupClose({ scheduleObj })}
         height="650px"
         ref={(schedule) => setScheduleObj(schedule)}
         eventSettings={{ dataSource: events }}
         dragStart={onDragStart}
       >
+        {console.log(scheduleObj)}
         <ViewsDirective>
           {["Day", "Week", "WorkWeek", "Month", "Agenda"].map((item) => (
             <ViewDirective key={item} option={item} />
